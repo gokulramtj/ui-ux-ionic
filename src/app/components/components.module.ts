df0006d813c2b215a38414cd9c8e5fc2c9b3e08e
gokulramtj <gokulram.t.j@gmail.com>
2020-05-13 19:05:54 +0530

    [ADD] Chat bot: Add blank form component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,9 +1,10 @@
 import { NgModule, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormComponent } from './form/form.component';
 
 
 const Components = [
-
+  FormComponent
 ]
 
 @NgModule({
