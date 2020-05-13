6530bd6575443031c6c1ddbe7b1b322d59144ba1
gokulramtj <gokulram.t.j@gmail.com>
2020-05-13 19:48:57 +0530

    [ADD] Chat bot: add form

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormComponent } from './form/form.component';
+import { IonicModule } from '@ionic/angular';
 
 
 const Components = [
@@ -12,7 +13,8 @@ const Components = [
     Components
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
   ],
   exports: [
     Components
