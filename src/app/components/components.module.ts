ebeb4a0f6b1e6f12bba1ed2d95e0ae2a145eb8ad
gokulramtj <gokulram.t.j@gmail.com>
2020-05-13 19:04:56 +0530

    [ADD] Chat bot: components module

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,20 @@
+import { NgModule, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+const Components = [
+
+]
+
+@NgModule({
+  declarations: [
+    Components
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    Components
+  ]
+})
+export class ComponentsModule { }
