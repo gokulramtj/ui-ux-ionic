5e3c858972c01c1cefc4c03a1b2a46ab967fe1f1
gokulramtj <gokulram.t.j@gmail.com>
2020-05-18 21:50:39 +0530

    [IMP] Chat bot: UI Tweaks and card view implementation
    On branch master

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,12 @@ import { NgModule, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormComponent } from './form/form.component';
 import { IonicModule } from '@ionic/angular';
+import { HomeContentComponent } from './home-content/home-content.component';
 
 
 const Components = [
-  FormComponent
+  FormComponent,
+  HomeContentComponent
 ]
 
 @NgModule({
